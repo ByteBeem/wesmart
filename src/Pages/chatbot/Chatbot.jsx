9b5dd085e683718a490a4d590c2fe49b826bbcc2
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-01-13 15:37:34 +0200

    Update Chatbot.jsx

--- src/Pages/chatbot/Chatbot.jsx
+++ src/Pages/chatbot/Chatbot.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect , useRef } from "react";
 import axios from "axios";
 import "./Chatbot.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
