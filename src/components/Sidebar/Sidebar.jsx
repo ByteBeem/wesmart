69dd61d6a5d94c49f31fbf6460911c07e08b48fc
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-02-05 00:33:47 +0200

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,9 @@
 import "./sidebar.scss";
 import "../../App.scss";
 import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
-import { FaUser } from "react-icons/fa";
-import { FiLoader } from "react-icons/fi";
-import { IoLogOut } from "react-icons/io5";
-import { GiWallet } from "react-icons/gi";
+import { PiTelevisionSimpleBold } from "react-icons/pi";
+import { FaDownload } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import { FaHandHoldingDollar } from "react-icons/fa6";
 import { useState } from "react";
 import { IoIosChatbubbles } from "react-icons/io";
@@ -14,35 +12,23 @@ const Sidebar = ({ active, closeSidebar }) => {
   const [loading, setLoading] = useState(false);
   const [activeItem, setActiveItem] = useState("home");
 
-  const handleLogout = () => {
-    setLoading(true);
-    localStorage.clear();
-    setTimeout(() => {
-      setLoading(false);
-      window.location.href = "https://peermine.vercel.app/login";
-      closeSidebar();
-    }, 5000);
-  };
+
   return (
     <aside className={`sidebar ${active}`}>
-      {loading && (
-        <div className="overlay">
-          <FiLoader className="loading-spinner" />
-          <p className="loading-text">Logging out...</p>
-        </div>
-      )}
+     
       <div className="top">
-        <h3>PeerMine</h3>
+        <h3>weSmart</h3>
         <div className="close_btn">&times;</div>
       </div>
+
 
       <div className="middle">
         <Link
           onClick={() => setActiveItem("home")}
           className={activeItem === "home" ? "link active" : "link"}
-          to="/dashboard"
+          to="/Home"
         >
-          <IoHome className="icon" />
+          <PiTelevisionSimpleBold className="icon" />
           <span>Home</span>
         </Link>
 
@@ -51,30 +37,22 @@ const Sidebar = ({ active, closeSidebar }) => {
           className={activeItem === "wallet" ? "link active" : "link"}
           to="/wallets"
         >
-          <GiWallet className="icon" />
-          <span>Wallet</span>
+          <FaSearch className="icon" />
+          <span>Search</span>
         </Link>
+
+      
 
         
 
         <Link className="link" to="/profile">
-          <FaUser className="icon" />
-          <span>Profile</span>
+          <FaDownload className="icon" />
+          <span>books</span>
         </Link>
 
         
 
-        <Link
-          className="link"
-          to=""
-          onClick={() => {
-            closeSidebar();
-            handleLogout();
-          }}
-        >
-          <IoLogOut className="icon" />
-          <span>Logout</span>
-        </Link>
+        
       </div>
     </aside>
   );
