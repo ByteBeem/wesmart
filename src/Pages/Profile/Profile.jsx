e1b58ccd1308efce778a0a7dc2726bcac5db7ea8
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-02-10 17:11:58 +0000

    new

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -6,12 +6,13 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import UserProfile from "../../assets/smart.jpg";
 import { Link } from "react-router-dom";
 import { FiLoader } from "react-icons/fi";
+import LoginModal from "./LoginModal"
 
 
 function Profile({ showSidebar, active, closeSidebar }) {
   
   const [userData, setUserData] = useState({});
- 
+  const [modalOpenLogin, setModalOpenLogin] = useState(false);
   const [loading, setLoading] = useState(false);
 
 
@@ -19,36 +20,50 @@ function Profile({ showSidebar, active, closeSidebar }) {
   const fullName = userData.name;
   const cellphone = userData.cell;
 
- 
+  
+
+  
+  const handleCloseModalLogin = () => {
+    
+    setModalOpenLogin(false);
+  };
+
 
   useEffect(() => {
     const token = localStorage.getItem("token");
 
+    if (!token) {
+        setModalOpenLogin(true);
+        return; 
+      }
+  
     if (token) {
-     
-
-      fetchUserData(token);
+      setLoading(true); 
+  
+      axios
+        .get("https://wesmart-3b311bc60078.herokuapp.com/getUserData", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => {
+          const info = response.data;
+          if (info !== undefined) {
+            setUserData(info); 
+          }
+        })
+        .catch((error) => {
+         
+          console.error("Error fetching user data:", error);
+        })
+        .finally(() => {
+          setLoading(false); 
+        });
     }
-  }, [token]);
+  }, []); 
+  
 
  
 
-  const fetchUserData = (token) => {
-    setLoading(true);
-    axios
-      .get("https://wesmart-3b311bc60078.herokuapp.com/getUserData", {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-         const info = response.data; 
-
-      if (info !== undefined) {
-        setUserData(info ); 
-      }
-        setLoading(false);
-      })
-      .catch((error) => {});
-  };
+  
 
   return (
     <div className="profile">
@@ -89,6 +104,13 @@ function Profile({ showSidebar, active, closeSidebar }) {
 
 
       </div>
+
+      {modalOpenLogin  && (
+        <>
+          <LoginModal onClose={handleCloseModalLogin} />
+          <button onClick={handleCloseModalLogin}>Close</button>
+        </>
+      )}
     </div>
   );
 }
