b51d1cc5b1fe7694080e4304c13d245e13a3714e
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-02-21 12:16:33 +0000

    new

--- src/Pages/Home/Modal.jsx
+++ src/Pages/Home/Modal.jsx
@@ -144,32 +144,6 @@ const Modal = ({ onClose, postId }) => {
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
-  
-        <div className="comments_container">
-          {loading ? (
-            <p>Loading...</p>
-          ) : comments.length === 0 ? (
-            <p>No comments available</p>
-          ) : (
-            comments.reverse().map((comment) => (
-              <div key={comment.id} className="comment_card">
-                {comment.content_type === "image" ? (
-                  <div>
-                    <p>{comment.caption}</p>
-                    <img
-                      src={comment.imageUrl}
-                      alt="Post"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  </div>
-                ) : comment.content_type === "text" ? (
-                  <p>{comment.caption}</p>
-                ) : null}
-              </div>
-            ))
-          )}
-        </div>
-  
         <div className="post_form">
           <form onSubmit={image ? handleSubmit : handleSubmitText}>
             <textarea
@@ -198,6 +172,33 @@ const Modal = ({ onClose, postId }) => {
             </button>
           </form>
         </div>
+  
+        <div className="comments_container">
+          {loading ? (
+            <p>Loading...</p>
+          ) : comments.length === 0 ? (
+            <p>No comments available</p>
+          ) : (
+            comments.reverse().map((comment) => (
+              <div key={comment.id} className="comment_card">
+                {comment.content_type === "image" ? (
+                  <div>
+                    <p>{comment.caption}</p>
+                    <img
+                      src={comment.imageUrl}
+                      alt="Post"
+                      style={{ maxWidth: "100%", height: "auto" }}
+                    />
+                  </div>
+                ) : comment.content_type === "text" ? (
+                  <p>{comment.caption}</p>
+                ) : null}
+              </div>
+            ))
+          )}
+        </div>
+  
+        
       </div>
     </div>,
     document.body
